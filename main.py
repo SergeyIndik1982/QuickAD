c239020777a20299a492624dd862aff1292db2f2
SergeyIndik1982 <duke@inbox.ru>
2026-02-02 10:21:31 +0100

    Rename CONFIG to CONFIG1 in main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pydantic import BaseModel
 import google.generativeai as genai
 
 # --------------------
-# CONFIG
+# CONFIG1
 # --------------------
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
